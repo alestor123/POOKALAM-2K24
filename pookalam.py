8e1972adc9622fc8dcfc2a6312973edd0f9bf4aa
Alestor Aldous <alestoraldous@gmail.com>
2024-09-20 11:30:43 +0530

    drawed circle

---
+++ pookalam.py
@@ -0,0 +1,19 @@
+import turtle
+import math
+# settings
+t = turtle.Turtle()
+outerRadius = 300
+innerRadius = 50
+
+
+# border 
+t.begin_fill()
+t.circle(innerRadius,360)
+t.pu()
+t.left(90)
+t.goto(outerRadius, 0)
+t.pd()
+t.circle(outerRadius,360)
+
+
+turtle.done()
