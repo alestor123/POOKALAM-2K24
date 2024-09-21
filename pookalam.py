604106471dc46c740a5908c220674fb2bd4a63c8
Alestor Aldous <alestoraldous@gmail.com>
2024-09-21 10:45:32 +0530

    initial pookalam

--- pookalam.py
+++ pookalam.py
@@ -3,15 +3,32 @@ import math
 # settings
 t = turtle.Turtle()
 t.speed(0)
-outerRadius = 290
+outerRadius = 300
 innerRadius = 50
-def petals(num, length):
+
+# functions for common shapes
+def drawCircle(color,radius): 
+    t.begin_fill()
+    t.fillcolor(color)
+    t.pu()
+    t.rt(90)
+    t.fd(radius)
+    t.left(90)
+    t.down()
+    t.circle(radius)
+    t.end_fill()
+    t.pu()
+    t.home()
+    t.down()
+
+def petals(num, length,color="none"):
     t.pu()
     t.home()
     t.pd()
     t.setheading(0)
     angle = 360 / num
     theta = 0
+    if color != "none": t.fillcolor(color)
     t.begin_fill()
     for i in range(num):
         t.setheading(theta)
@@ -19,73 +36,30 @@ def petals(num, length):
         theta += angle
     t.end_fill()
 
-# common shapes
-def drawSquare(radius,color,degree=0) :
-# since diagonal of square is side x √2 
-    sideLength = radius * math.sqrt(2)  
-    t.fillcolor(color)
-    t.begin_fill()
-    t.penup()
-    t.goto(-sideLength/2, sideLength/2)
-    t.pendown()
-    for _ in range(4):
-        t.backward(sideLength)
-        t.left(90)
-    t.end_fill()
+def coverFlower(color,radius):
+    for i in range(13):
+        t.up()
+        t.goto(0,0)
+        t.down()
+        t.fillcolor(color)
+        t.pen(pencolor=color)
+        t.begin_fill()
+        t.circle(radius,70)
+        t.left(110)
+        t.circle(radius,70)
+        t.end_fill()
+        t.right(1)
 
-
-# border (DRAWING CIRCLE)
-t.fillcolor("magenta")
-t.begin_fill()
-t.pu()
-t.goto(0, -50) 
-t.pd()
-t.circle(innerRadius,360)
-t.end_fill()
-# inner circle
-
-
-
-t.pu()
-t.left(90)
-t.goto(outerRadius, 0)
-t.pd()
-t.pendown()
-t.circle(outerRadius,360)
-
-# t.fillcolor("dark blue")
-# t.begin_fill()
-# t.end_fill()
-
-# outer lining :
-t.fillcolor("maroon")
-t.begin_fill()
-
-t.pu()
-t.left(360)
-t.goto(outerRadius+10, 0)
-t.pd()
-t.pendown()
-t.circle(outerRadius+10,360)
-# outer circle :
-
-t.pu()
-t.left(360)
-t.goto(outerRadius+15, 0)
-t.pd()
-t.pendown()
-t.circle(outerRadius+15,360)
-t.end_fill()
-petals(5,innerRadius)
-
-# t.home()
-
-# drawSquare(outerRadius,"magenta")
-
-
-
-
-
+drawCircle("navy",outerRadius+10)
+drawCircle("#FF8C00",outerRadius)
+coverFlower("#FAE033",outerRadius-39)
+petals(5,innerRadius+30,"magenta")
+drawCircle("yellow",innerRadius)
+coverFlower("navy",innerRadius-10)
+t.color("gold")
+style = ("monospace",15)
+t.write("\n FOSS",font=style,align="center")
+t.hideturtle()
 turtle.exitonclick()
 
 
